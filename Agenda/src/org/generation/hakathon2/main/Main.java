a68cc7d7ed151917817fb5f8c9a8478bb0c8b725
Elias López Martínez <91902204+DIEGOELIASLOPEZ@users.noreply.github.com>
2026-08-03 16:02:52 -0600

    Merge Completados

--- Agenda/src/org/generation/hakathon2/main/Main.java
+++ Agenda/src/org/generation/hakathon2/main/Main.java
@@ -13,14 +13,6 @@ public class Main {
 
     public static void main(String[] args) {
         Agenda agenda = new Agenda();
-        Contact contact2 = new Contact("Jesus", "Hernandez", "5564373152");
-        agenda.addContact(contact2);
-        Contact contact1 = new Contact("Elias", "Lopez", "5564275142");
-        agenda.addContact(contact1);
-
         agenda.menu();
-
-
-
     }
 }
