e0e502705b96f365bfd0f2d408f1a9fba85ce4b7
Elias López Martínez <91902204+DIEGOELIASLOPEZ@users.noreply.github.com>
2026-08-03 16:51:31 -0600

    solucion?

---
+++ AgendaHakathon2/src/generation/hakathon2/main/Main.java
@@ -0,0 +1,11 @@
+package generation.hakathon2.main;
+
+import generation.hakathon2.agenda.Agenda;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Agenda agenda = new Agenda();
+        agenda.menu();
+    }
+}
