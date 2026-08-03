aa7b8d448fb3378367448ead78558933f9c186b5
SantiagoJimenez05 <santiago_20@gmail.com>
2026-08-03 12:57:03 -0600

    Modificación clase agenda

--- Agenda/src/org/generation/hakathon2/agenda/Agenda.java
+++ Agenda/src/org/generation/hakathon2/agenda/Agenda.java
@@ -1,4 +1,48 @@
 package org.generation.hakathon2.agenda;
 
 public class Agenda {
+    private List<Contact> agenda = new Arraylist<>();
+    private final int maxContact = 10;
+
+    public Agenda(){
+
+    }
+
+    public List<Contact> getAgenda() {
+        return agenda;
+    }
+    public int getMaxContact(){
+        return maxContact;
+    }
+    //1. Añadir contacto - Margarita
+    public void addContact(Contact c){
+
+    }
+    //2. Verifica si existe Contacto - Santiago
+    public boolean existContact(Contact c){
+
+    }
+    //3. Listar contactos - Elías
+    public void listContact(){
+
+    }
+    //4. Buscar contacto - Elías
+    public void searchContact(){
+    }
+    //5. Eliminar contacto - Jesús
+    public void removeContact(){
+
+    }
+    //6. Modificar telefono - Andrea
+    public void updateTel(String nombre, String apellido, String nuevoTelefono){
+
+    }
+    //7. Agenda llena - Andrea
+    public void fullAgenda(){
+
+    }
+    //8. Espacio disponible - Adri
+    public void freeSpace(){
+
+    }
 }
