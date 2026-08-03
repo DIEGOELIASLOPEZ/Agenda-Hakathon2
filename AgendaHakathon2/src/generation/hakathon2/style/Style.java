78eb1d0e368d1c19ca8b1ac01db3ff4213893ef6
Elias López Martínez <91902204+DIEGOELIASLOPEZ@users.noreply.github.com>
2026-08-03 17:00:12 -0600

    Correccion de Errores

---
+++ AgendaHakathon2/src/generation/hakathon2/style/Style.java
@@ -0,0 +1,23 @@
+package generation.hakathon2.style;
+
+public class Style {
+
+    public static final String RESET = "\u001B[0m";
+    public static final String BOLD = "\u001B[1m";
+
+    // Colores de texto
+    public static final String CYAN = "\u001B[36m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String RED = "\u001B[31m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String GRAY = "\u001B[90m";
+
+    // Método para limpiar la consola en Windows
+    public static void limpiarPantalla() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+}
